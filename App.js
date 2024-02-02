b5ba2ebb21c54514bb655977ac1c8f725bc3f97e
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2024-02-02 23:04:05 +0100

    feat: implement navigation

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Importeer de navigatiebibliotheek
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Importeer de schermen
+import LoginScreen from './screens/Login';
+
+// Maak een nieuw stapelnavigator
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen}options={{headerShown: false,}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-} 
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
