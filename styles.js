4c5467eda1cbd0e15a31d7b76a63236cbd95e374
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2024-02-18 15:10:12 +0100

    chore: add global styles for optimalisation

---
+++ styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+    headingOne:{
+        color: colors.offblack,
+    },
+    headingTwo:{
+        color: colors.offblack,
+    },
+    headingThree:{
+        color: colors.offblack,
+    },
+    link:{
+        color: colors.purple,
+    },
+    bodyText:{
+        color: colors.offblack,
+    },
+});
+
+export default styles;
